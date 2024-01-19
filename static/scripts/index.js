f8a6bae1625e93e2487d672f8b3905588c17dccb
jan-frh <jan.frueh@outlook.de>
2024-01-19 21:56:43 +0100

    Exchang the old layout of the website with a css grid in the form of a 2x2 matrix. Additionally add comments to the new blocks.

--- static/scripts/index.js
+++ static/scripts/index.js
@@ -19,62 +19,66 @@ let clickCountTemp = 0;
 let fourthbutton = false;
 let clickCountCalo = 0;
 
-//Function which splits the website for the given parameter "e" in the wanted cells.
-function Split(e) {
-    if (e == 2) heartrate.style.display = "block";
-    if (e == 3) temperature.style.display = "block";
-    if (e == 4) calories.style.display = "block";
+
+
+//Function which sets a graph-container visible through the ID.
+function displayRow(item) {
+    var currentHiddenRow = document.getElementById(item);
+    if (currentHiddenRow.classList.contains('hidden-row')) currentHiddenRow.classList.remove('hidden-row');
 }
+
+
 
 //Code block will be executed when the button "Steps" is pressed.
 document.getElementById("Steps").addEventListener("click", () => {
     //Count up for every click on the button.
     clickCountSteps++;
-    //Call the plot "steps" with its ID.
-    const steps = document.getElementById("steps");
+    //Call the graph-container "steps" with its ID.
+    var steps = document.getElementById("steps");
 
     //When you activate the button "Steps" (uneven number of clicks), this branch will be executed.
     if (clickCountSteps % 2 != 0) {
         firstbutton = true;
         //Change color of button to "lightblue", when pressed.
         document.getElementById("Steps").style.backgroundColor = "lightblue";
-        //Activate the cell with the content of the plot.
-        steps.style.display = "block";
+        //Activate the graph-container with the content of the plot.
+        steps.classList.remove('hidden-row');
 
         // Generate graph from steps.js
-        generateStepsGraph();
+        if (clickCountSteps== 1) generateStepsGraph();
 
-        //Distinguish which combination of buttons is pressed and call the function "Split".
-        if (firstbutton && secondbutton) Split(2);
-        if (firstbutton && thirdbutton) Split(3);
-        if (firstbutton && fourthbutton) Split(4);
+        //Distinguish which combination of buttons is pressed and call the function "displayRow".
+        if (firstbutton && secondbutton) displayRow('heartrate');
+        if (firstbutton && thirdbutton) displayRow('temperature');
+        if (firstbutton && fourthbutton) displayRow('calories');
 
         //When you deactivate the button "Steps" (even number of clicks), this branch will be executed.
     } else {
         firstbutton = false;
         //Change color of button to "white", when release the button.
         document.getElementById("Steps").style.backgroundColor = "white";
-        //Deactivate the cell with the plot of the button.
-        steps.style.display = "none";
+        //Deactivate the graph-container with the plot of the button.
+        steps.classList.add('hidden-row');
     }
 });
 
 //Functionality of button "Heartrate". For more information, see code block of button "Steps".
 document.getElementById("Heartrate").addEventListener("click", () => {
     clickCountHeartrate++;
-    const heartrate = document.getElementById("heartrate");
+    var heartrate = document.getElementById("heartrate");
 
     if (clickCountHeartrate % 2 != 0) {
         secondbutton = true;
-        heartrate.style.display = "block";
+        displayRow('heartrate');
+        heartrate.classList.remove('hidden-row');
         document.getElementById("Heartrate").style.backgroundColor = "lightblue";
 
         // Generate graph from heartrate.js
-        generateHeartrateGraph();
-        if (firstbutton && secondbutton) Split();
+        if (clickCountHeartrate== 1) generateHeartrateGraph();
+        if (firstbutton && secondbutton) displayRow();
     } else {
         secondbutton = false;
-        heartrate.style.display = "none";
+        heartrate.classList.add('hidden-row');
         document.getElementById("Heartrate").style.backgroundColor = "white";
     }
 });
@@ -82,20 +86,19 @@ document.getElementById("Heartrate").addEventListener("click", () => {
 //Functionality of button "Temperature". For more information, see code block of button "Steps".
 document.getElementById("Temperature").addEventListener("click", () => {
     clickCountTemp++;
-    const temperature = document.getElementById("temperature");
+    var temperature = document.getElementById("temperature");
 
     if (clickCountTemp % 2 != 0) {
         thirdbutton = true;
-        temperature.style.display = "block";
+        temperature.classList.remove('hidden-row');
         document.getElementById("Temperature").style.backgroundColor = "lightblue";
 
         // Generate graph from temperature.js
-        generateTemperatureGraph();
-
-        if (firstbutton && secondbutton) Split();
+        if (clickCountTemp== 1) generateTemperatureGraph();
+        if (firstbutton && thirdbutton) displayRow();
     } else {
         thirdbutton = false;
-        temperature.style.display = "none";
+        temperature.classList.add('hidden-row');
         document.getElementById("Temperature").style.backgroundColor = "white";
     }
 });
@@ -103,20 +106,19 @@ document.getElementById("Temperature").addEventListener("click", () => {
 //Functionality of button "Calories". For more information, see code block of button "Steps".
 document.getElementById("Calories").addEventListener("click", () => {
     clickCountCalo++;
-    const calories = document.getElementById("calories");
+    var calories = document.getElementById("calories");
 
     if (clickCountCalo % 2 != 0) {
         fourthbutton = true;
-        calories.style.display = "block";
+        calories.classList.remove('hidden-row');
         document.getElementById("Calories").style.backgroundColor = "lightblue";
 
         // Generate graph from calories.js
-        generateCaloriesGraph();
-
-        if (firstbutton && secondbutton) Split();
+        if (clickCountCalo== 1) generateCaloriesGraph();
+        if (firstbutton && fourthbutton) displayRow();
     } else {
         fourthbutton = false;
-        calories.style.display = "none";
+        calories.classList.add('hidden-row');
         document.getElementById("Calories").style.backgroundColor = "white";
     }
 });
